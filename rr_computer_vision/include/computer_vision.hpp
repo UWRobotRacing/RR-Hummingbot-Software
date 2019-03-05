82d5f76b763faa8fadf96ff3a2dac992882d0745
Waleed Ahmed <wahmedwd3@gmail.com>
2019-03-04 22:50:55 -0500

    Zed Integration (#3)
    * Added in the callbacks; TODO: ZED Camera Testing
    
    * Fixed zed-ros-wrapper library package
    
    * Cleaned up computer vision launch file to only launch base zed wrapper
    
    * Changed named name to rr_computer_vision for consistency
    
    * Changed name

---
+++ rr_computer_vision/include/computer_vision.hpp
@@ -0,0 +1,36 @@
+/** @file computer_vision.hpp
+ *  @author Andrew Jin (D29Jin)
+ *  @competition IARRC 2019
+ */
+#ifndef COMPUTER_VISION
+#define COMPUTER_VISION
+
+
+//ROS
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+
+//OPENCV includes
+#include <opencv2/opencv.hpp>
+
+//LOCAL
+
+class ComputerVision
+{
+  public:
+    ComputerVision(ros::NodeHandle nh);
+    ~ComputerVision();
+    void InitializeSubscribers();
+  private:
+    void RGBCameraCallback(const sensor_msgs::Image& msg);
+    void LeftCameraCallback(const sensor_msgs::Image& msg);
+    void RightCameraCallback(const sensor_msgs::Image& msg);
+    void DepthCameraCallback(const sensor_msgs::Image& msg);
+    ros::Subscriber rgb_camera_subscriber_;
+    ros::Subscriber left_camera_subscriber_;
+    ros::Subscriber right_camera_subscriber_;
+    ros::Subscriber depth_camera_subscriber_;
+    ros::NodeHandle nh_;
+};
+
+#endif // COMPUTER_VISION
