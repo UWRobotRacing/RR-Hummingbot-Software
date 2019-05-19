ef5f1c2933460c1e95dc9fe3aa8ace0aab0e2bb3
¨Allan¨ <556zhao@edu.uwaterloo.ca>
2019-05-18 21:18:43 -0400

    Added endline header and source file from last years codebase

---
+++ rr_computer_vision/include/endline_detection.hpp
@@ -0,0 +1,33 @@
+/** @file endline_detection.hpp
+ * @author Angela Gu (angegu)  && Yuchi Zhao
+ * @author Toni Ogunmade (oluwatoni)
+ */
+
+#ifndef ENDLINE_DETECTION_HPP
+#define ENDLINE_DETECTION_HPP
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/opencv.hpp>
+#include <std_srvs/Trigger.h>
+#include "msg_srv_names.hpp"
+
+class EndlineCounter {
+  private :
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    
+    ros::ServiceClient client_;
+    bool detection_status_;
+    int hysteresis_counter_;
+    int hysteresis_constant_;
+  
+  public :
+    EndlineCounter(ros::NodeHandle);
+    void ImgCb(const sensor_msgs::ImageConstPtr&);
+    bool BlobDetector(cv::Mat);
+};
+
+#endif /*ENDLINE_DETECTION_HPP*/
