04615abeace9bcb30e710925d0989f5b1b2b05cc
Waleed Ahmed <wahmedwd3@gmail.com>
2019-05-29 23:24:11 -0400

    Tried to implemen synchronized policy but I get a million compile errors

--- rr_computer_vision/include/computer_vision.hpp
+++ rr_computer_vision/include/computer_vision.hpp
@@ -9,6 +9,8 @@
 //ROS
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
 
 //OPENCV includes
 #include <opencv2/opencv.hpp>
@@ -22,13 +24,19 @@ class ComputerVision
     ~ComputerVision();
     void InitializeSubscribers();
   private:
-    void RGBCameraCallback(const sensor_msgs::Image& msg);
+    //void RGBCameraCallback(const sensor_msgs::Image& msg);
+    void LeftRightSyncCameraCallback(const sensor_msgs::Image& left_msg, const sensor_msgs::Image& right_msg);
     void LeftCameraCallback(const sensor_msgs::Image& msg);
     void RightCameraCallback(const sensor_msgs::Image& msg);
     void DepthCameraCallback(const sensor_msgs::Image& msg);
     ros::Subscriber rgb_camera_subscriber_;
-    ros::Subscriber left_camera_subscriber_;
-    ros::Subscriber right_camera_subscriber_;
+
+    message_filters::Subscriber<sensor_msgs::Image> left_camera_subscriber_;
+    message_filters::Subscriber<sensor_msgs::Image> right_camera_subscriber_;
+    TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image> sync;
+
+    //ros::Subscriber left_camera_subscriber_;
+    //ros::Subscriber right_camera_subscriber_;
     ros::Subscriber depth_camera_subscriber_;
     ros::NodeHandle nh_;
 };
--- rr_computer_vision/src/computer_vision.cpp
+++ rr_computer_vision/src/computer_vision.cpp
@@ -11,7 +11,14 @@
  * @param nh: ROS node handler
 */
 ComputerVision::ComputerVision(ros::NodeHandle nh) {
+  left_camera_subscriber_.subscribe(nh, "left/image_rect_color", 1);
+  right_camera_subscriber_.subscribe(nh, "right/image_rect_color", 1);
+  TimeSynchronizer<Image, CameraInfo> sync(image_sub, info_sub, 10);
+  sync.registerCallback(boost::bind(&LeftRightSyncCameraCallback, _1, _2));
 
+  // message_filters::Subscriber<sensor_msgs::Image> left_camera_subscriber_(nh, "left/image_rect_color", 1);
+  // message_filters::Subscriber<sensor_msgs::Image> right_camera_subscriber_(nh, "right/image_rect_color", 1);
+  // TimeSynchronizer<Image, CameraInfo> sync(image_sub, info_sub, 10);
 }
 
 /*
@@ -28,23 +35,27 @@ ComputerVision::~ComputerVision() {
 */
 void ComputerVision::InitializeSubscribers() {
     rgb_camera_subscriber_ = nh_.subscribe("rgb/image_rect_color", 0, &ComputerVision::RGBCameraCallback, this);
-    left_camera_subscriber_ = nh_.subscribe("left/image_rect_color", 0, &ComputerVision::LeftCameraCallback, this);
-    right_camera_subscriber_ = nh_.subscribe("right/image_rect_color", 0, &ComputerVision::RightCameraCallback, this);
+    //left_camera_subscriber_ = nh_.subscribe("left/image_rect_color", 0, &ComputerVision::LeftCameraCallback, this);
+    //right_camera_subscriber_ = nh_.subscribe("left/image_rect_color", 0, &ComputerVision::RightCameraCallback, this);
     depth_camera_subscriber_ = nh_.subscribe("depth/depth_registered", 0, &ComputerVision::DepthCameraCallback, this);
 }
 
-void ComputerVision::RGBCameraCallback(const sensor_msgs::Image& msg){
+// void ComputerVision::RGBCameraCallback(const sensor_msgs::Image& msg){
+
+// }
+
+void ComputerVision::LeftRightSyncCameraCallback(const sensor_msgs::Image& left_msg, sensor_msgs::Image& right_msg) {
 
 }
 
-void ComputerVision::LeftCameraCallback(const sensor_msgs::Image& msg){
+void ComputerVision::LeftCameraCallback(const sensor_msgs::Image& msg) {
     
 }
 
-void ComputerVision::RightCameraCallback(const sensor_msgs::Image& msg){
+void ComputerVision::RightCameraCallback(const sensor_msgs::Image& msg) {
     
 }
 
-void ComputerVision::DepthCameraCallback(const sensor_msgs::Image& msg){
+void ComputerVision::DepthCameraCallback(const sensor_msgs::Image& msg) {
     
 }
