7e4c0eff6b2188762d4b5084a74d756b2bf6f1ff
¨Allan¨ <556zhao@edu.uwaterloo.ca>
2019-07-06 21:56:14 -0400

    delete files that is not used this year

--- rr_supervisor/include/supervisor.hpp
+++ rr_supervisor/include/supervisor.hpp
@@ -10,25 +10,18 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/Vector3.h>
-#include <math.h>
-#include <time.h>
-#include <fstream>
-#include <sstream>
 #include ¨msg_srv_names.hpp¨
-
-
 
 class Supervisor
 {
   public:
     //constructor & distructor
     Supervisor();
-    Supervisor::~Supervisor() 
+    Supervisor::~Supervisor();
+
     // Callback methods
     bool StartRace(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
     bool CountLap(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
-    void TrackSpeed(const geometry_msgs::TwistConstPtr& msg);
-    void MonitorBattery(const std_msgs::Int8::ConstPtr& msg);
     void FinishRace();
     
     // other methods & Variables
@@ -40,8 +33,6 @@ class Supervisor
     ros::NodeHandle nh_;
     ros::Publisher twist_pub_;
     ros::Publisher null_lock_;
-    ros::Subscriber cmd_sub_;
-    ros::Subscriber battery_sub_;
     ros::ServiceServer start_race_service_;
     ros::ServiceServer count_lap_service_;
 
@@ -50,12 +41,8 @@ class Supervisor
     geometry_msgs::Twist twist_msg_;
     geometry_msgs::Vector3 null_vector_;
     
-    clock_t begin_time_;
     int lap_count_;
     int twist_msg_count_;
-    float speed_sum_;
-    float race_time_;
-    float average_speed_;
 };
 
 #endif  // SUPERVISOR_H
--- rr_supervisor/src/supervisor.cpp
+++ rr_supervisor/src/supervisor.cpp
@@ -12,7 +12,6 @@
  *
  *  Topics Subscribed:
  *    /rr_vehicle/vel_cmd
- *    /arduino/battery_state
  *  @author Waleed Ahmed(w29ahmed) && Yuchi(Allan) Zhao
  *  @competition IARRC 2019
  */
@@ -20,7 +19,6 @@
 #include <supervisor.hpp>
 
 using namespace supervisor_topic;
-using namespace arduino_topic;
 using namespace rr_cmd_topics;
 
 /**
@@ -38,10 +36,6 @@ Supervisor::Supervisor()
       // Setup publishers for twist multiplexer
     twist_pub_ = nh_.advertise<geometry_msgs::Twist>(cmd, 1);
     null_lock_ = nh_.advertise<std_msgs::Bool>(no_movement, 1);
-
-    // Setup subscribers to monitor battery and speed
-    cmd_sub_ = nh_.subscribe(vel_cmd, 1, &Supervisor::TrackSpeed, this);
-    battery_sub_ = nh_.subscribe(battery_state, 1, &Supervisor::MonitorBattery, this);
 
     // Setup service servers
     start_race_service_ = nh_.advertiseService(start_race, &Supervisor::StartRace, this);
@@ -84,25 +78,24 @@ bool Supervisor::CountLap(std_srvs::Trigger::Request &req, std_srvs::Trigger::Re
 {
     if(race_type==¨drag¨)
     {
-        lap_count_+=1;
         res.success=true;
         ROS_INFO(¨Drag race complete! shutting down¨)
     }
     else if(race_type_==¨circuit¨)
     {
-        if (lap_count_ < 3)
+        if (lap_count_ < 1)
         {
             lap_count_ += 1;
-            if (lap_count_ == 3)
+            if (lap_count_ == 1)
             {
                 res.success = true;
                 ROS_INFO("Circuit race complete! shutting down");
             }
-            else
-            {
-                res.success = false;
-                ROS_INFO("Lap %d of %d complete!", lap_count_, 3);
-            }
+            // else
+            // {
+            //     res.success = false;
+            //     ROS_INFO("Lap %d of %d complete!", lap_count_, 3);
+            // }
         }
     }
 
@@ -113,53 +106,9 @@ bool Supervisor::CountLap(std_srvs::Trigger::Request &req, std_srvs::Trigger::Re
     }
     return true;
 }
-void Supervisor::TrackSpeed(const geometry_msgs::TwistConstPtr& msg)
-{
-    twist_msg_count_+=1;
-    speed_sum_+=(pow(msg->linear.x, 2) + pow(msg->linear.y, 2))
-}
-
-void Supervisor::MonitorBattery(const std_msgs::Int8::ConstPtr& msg)
-{
-  if (msg->data <= 10)
-  {
-    ROS_INFO("Battery is less than 10 percent, ending race...");
-    this->FinishRace();
-  }
-}
 
 void Supervisor::FinishRace()
 {
-  // Calculate average speed and elapsed time since start of race
-  average_speed_ = speed_sum_ / twist_msg_count_;
-  race_time_ = float(clock() - begin_time_) / CLOCKS_PER_SEC;
-
-  // Construct a timestamp with the following format:
-  // Year_Month_day--Hours_minutes_seconds
-  time_t rawtime;
-  struct tm * time_info;
-  char date_string[50];
-  time(&rawtime);
-  time_info = localtime(&rawtime);
-  strftime(date_string, 50, "%Y_%m_%d---%H_%M_%S", time_info);
-
-  // Open a file to write to
-  std::ofstream race_metrics_file;
-  std::ostringstream file_name;
-  file_name << "~/" << race_type_ << "_" << date_string << ".txt";
-  race_metrics_file.open(file_name.str().c_str());
-
-  // Write to file
-  std::ostringstream metrics;
-
-  metrics << "Date: " << date_string << "\n";
-  metrics << "Race type: " << race_type_ << "\n";
-  metrics << "Race time: " << race_time_ << "\n";
-  metrics << "Average speed: " << average_speed_ << "\n";
-
-  race_metrics_file << metrics.str();
-  race_metrics_file.close();
-
   // Publish null vector to twist multiplexer and lock out any other messages
   // from being published, which will stop the robot
   twist_pub_.publish(twist_msg_);
